eed688bc581b17f25488dfb57781c98c82a09298
Diana Rose Mesquita Lobo <diana.lobo@tr.abastece.com>
2024-01-15 15:33:11 -0300

    feature: Autenticação

---
+++ src/main/java/med/voll/api/infra/SecurityConfigurations.java
@@ -0,0 +1,45 @@
+package med.voll.api.infra;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+// Indicar para o spring que vai ser personalizado as configurações de segurança.
+@EnableWebSecurity
+public class SecurityConfigurations {
+
+    /*  Bean: Essa anotação serve para expor um retorno desse método.
+        Devolve um objeto para Spring.
+        Ou para poder fazer injeção em algum controller ou classe service
+     */
+    @Bean
+    // Configuração do processo de atutenticação, passar de statefull para stateless.
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+
+    /*  Bean: serve para exportar uma classe para o Spring,
+        fazendo com que ele consiga carregá-la e realize a sua injeção
+        de dependência em outras classes.
+    */
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
